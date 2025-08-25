6d489be9022a7be85bbb56b7ae0465dc1359f9ee
Nikol Mrazova <nikolikrat@gmail.com>
2025-08-25 17:02:55 +0200

    feat: add a test for scenario 5 - add a book to users collection via API and verify

--- tests/apiTests.spec.ts
+++ tests/apiTests.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect, request } from "@playwright/test";
 import { BookSchema, BooksResponseSchema } from "../schemas/bookSchema";
+import 'dotenv/config';
 
 test("Scenario 4: Verify Book List API Response Status and Schema", async () => {
   const apiContext = await request.newContext();
@@ -20,4 +21,33 @@ test("Scenario 4: Verify Book List API Response Status and Schema", async () => 
       BookSchema.parse(book);
     });
   });
+});
+
+test("Scenario 5: Add a Book to User's Collection via API and verify", async () => {
+  const chosenIsbn = '9781449325862'
+  const apiContext = await request.newContext({ baseURL: 'https://demoqa.com' });
+  const loginResponse = await apiContext.post('/Account/v1/Login', {
+    data: { userName: process.env.USER_NAME, password: process.env.USER_PASSWORD }
+  });
+  const loginData = await loginResponse.json();
+  const { userId, token } = loginData;
+  const authContext = await request.newContext({
+    baseURL: 'https://demoqa.com',
+    extraHTTPHeaders: {
+      Authorization: `Bearer ${token}`,
+      'Content-Type': 'application/json'
+    }
+  });
+  const addBookResp = await authContext.post('/BookStore/v1/Books', {
+    data: {
+      userId: userId,
+      collectionOfIsbns: [{ isbn: chosenIsbn }]
+    }
+  });
+  expect(addBookResp.ok()).toBeTruthy();
+  const getCollectionResp = await authContext.get(`/BookStore/v1/Books?UserId=${userId}`);
+  expect(getCollectionResp.ok()).toBeTruthy();
+  const collectionData = await getCollectionResp.json();
+  const isbnsInCollection = collectionData.books.map((b: any) => b.isbn);
+  expect(isbnsInCollection).toContain(chosenIsbn);
 });
